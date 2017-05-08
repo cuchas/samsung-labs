c2c495cf3e1286826af6d52ab1e5e0c4c41be69b
Eduardo Cucharro <eduardo.cucharro@gmail.com>
2017-05-08 11:56:26 -0300

    listview com os cursos de android

--- A4Demo/app/src/main/java/com/example/eduardocucharro/a4demo/MainActivity.java
+++ A4Demo/app/src/main/java/com/example/eduardocucharro/a4demo/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.eduardocucharro.a4demo;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        String[] array = getResources().getStringArray(R.array.cursos);
+        List<String> cursos = Arrays.asList(array);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cursos);
+
+        setListAdapter(adapter);
     }
 }
